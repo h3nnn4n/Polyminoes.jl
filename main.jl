f5a3dc17ab3c7b65f65313021fb360c9159282a5
h3nnn4n <uber.renan@gmail.com>
2015-08-31 23:01:56 -0300

    Old poolSolve as poolSolveOld + benchmarking function + random testing stuff

--- main.jl
+++ main.jl
@@ -1,3 +1,5 @@
+output = false
+
 type Node
     cubes      :: Array{(Int64, Int64)}
 end
@@ -66,10 +68,8 @@ function canFit(board :: Board, piece :: Node, x :: Int64, y :: Int64)
         posx, posy = x + p[1] - 1, y + p[2] - 1
         if posx > bx || posy > by
             return false
-        else
-            if board.squares[posx, posy] != 0
-                return false
-            end
+        elseif board.squares[posx, posy] != 0
+            return false
         end
     end
 
@@ -119,7 +119,46 @@ function isFilled(board :: Board)
     return true
 end
 
-function solvePool(board :: Board, pool :: Array{Piece}, lvl :: Int64 = 1, poolCtrl = None)
+function solvePoolOld(board :: Board, pool :: Array{Piece}, lvl :: Int64 = 1)
+    x, y = board.size
+
+    if lvl == 1 && length(pool) * 4 != x * y
+        println("Error")
+        println(length(pool) * 4, " != ", x*y)
+    end
+
+    for i in 1:x, j in 1:y
+        for (n, pp) in enumerate(pool)
+            for p in pp.cubits
+
+                if canFit(board, p, i, j)
+                    newBoard = deepcopy(board)
+                    newPool  = deepcopy(pool)
+                    putPiece(newBoard, p, pp.id, i, j)
+
+                    splice!(newPool, n)
+                    if length(newPool) == 0 && isFilled(newBoard)
+                        if output
+                            println(newBoard)
+                        end
+                        return true
+                    end
+
+                    result = solvePoolOld((newBoard), newPool, lvl + 1)
+
+                    if result
+                        board = newBoard
+                        return true
+                    end
+                end
+            end
+        end
+    end
+
+    return false
+end
+
+function solvePool(board :: Board, pool :: Array{Piece}, lvl :: Int64 = 1, poolCtrl = [])
     x, y = board.size
     iter = 0
     poolSize = length(pool)
@@ -138,10 +177,8 @@ function solvePool(board :: Board, pool :: Array{Piece}, lvl :: Int64 = 1, poolC
     for i in 1:x, j in 1:y
         for (n, pp) in enumerate(pool)
             for np in 1:poolSize
-                #=println(np, " ", poolControl, " ", lvl)=#
                 if poolControl[np]
                     piece = pool[np]
-                    #=println(piece)=#
                     iter += 1
 
                     for p in piece.cubits
@@ -149,16 +186,16 @@ function solvePool(board :: Board, pool :: Array{Piece}, lvl :: Int64 = 1, poolC
                             poolControl[np] = false
                             putPiece(board, p, piece.id, i, j)
 
-                            if isFilled(board)
-                                q  = false
-                                for w in poolControl
-                                    q |= w
+                            q = true
+                            for w in poolControl
+                                if w
+                                    q = false
                                 end
+                            end
 
-                                if !q
-                                    println(board)
-                                    return true
-                                end
+                            if q && isFilled(board)
+                                output && println(board)
+                                return true
                             end
 
                             result = solvePool(board, pool, lvl + 1, poolControl)
@@ -179,36 +216,91 @@ function solvePool(board :: Board, pool :: Array{Piece}, lvl :: Int64 = 1, poolC
     return false
 end
 
+function tester(board, pool)
+    b1 = deepcopy(board)
+    b2 = deepcopy(board)
+
+    p1 = deepcopy(pool)
+    p2 = deepcopy(pool)
+
+    #=println("-------------------")=#
+    #=@time solvePool(b1, p1)=#
+    #=@time solvePoolOld(b2, p2)=#
+    #=println("-------------------")=#
+
+    tic()
+    #=@profile solvePool(b1, p1)=#
+    solvePool(b1, p1)
+    t1 = toc()
+
+    #=Profile.print()=#
+
+    tic()
+    solvePoolOld(b2, p2)
+    t2 = toc()
+
+    return t1, t2
+end
+
 function main()
-    #=board = getRectangleBoard(8, 5)=#
-    #=pool  = Piece[]=#
+    board = getRectangleBoard(8, 5)
+    pool  = Piece[]
 
-    #=push!(pool, triangle)=#
-    #=push!(pool, triangle)=#
-    #=push!(pool, lllll)=#
-    #=push!(pool, sssss)=#
-    #=push!(pool, zzzzz)=#
-    #=push!(pool, triangle)=#
-    #=push!(pool, triangle)=#
-    #=push!(pool, lllll)=#
-    #=push!(pool, sssss)=#
-    #=push!(pool, zzzzz)=#
+    push!(pool, triangle)
+    push!(pool, triangle)
+    push!(pool, lllll)
+    push!(pool, sssss)
+    push!(pool, zzzzz)
+    push!(pool, triangle)
+    push!(pool, triangle)
+    push!(pool, lllll)
+    push!(pool, sssss)
+    push!(pool, zzzzz)
 
+    tester(board, pool)
     #=solvePool(board, pool)=#
 
-    #=return=#
+    return
 
-    x, y = 4, 4
+    #=x, y = 4, 5=#
+    #=board = getRectangleBoard(x, y)=#
+    #=pool = [Piece([Node([(1,1),(2,1),(2,2),(2,3)]),Node([(1,2),(2,2),(3,2),(3,1)]),Node([(1,1),(1,2),(1,3),(2,3)]),Node([(1,2),(1,1),(2,1),(3,1)])],4,4,7),=#
+            #=Piece([Node([(1,1),(2,1),(3,1),(4,1)]),Node([(1,1),(1,2),(1,3),(1,4)])],2,4,2),=#
+            #=Piece([Node([(1,1),(2,1),(3,1),(4,1)]),Node([(1,1),(1,2),(1,3),(1,4)])],2,4,2),=#
+            #=Piece([Node([(1,1),(1,2),(1,3),(2,1)]),Node([(1,1),(2,1),(3,1),(3,2)]),Node([(1,3),(2,3),(2,2),(2,1)]),Node([(1,1),(1,2),(2,2),(3,2)])],4,4,6),=#
+            #=Piece([Node([(2,1),(2,2),(1,2),(3,1)]),Node([(1,1),(1,2),(2,2),(2,3)])],2,4,5)]=#
+
+    #=t1, t2 = tester(board, pool)=#
+    #=println("$t1 $t2")=#
+
+    return
+
+    x, y = 4, 5
 
     board = getRectangleBoard(x, y)
     pool  = Piece[]
 
-    push!(pool, triangle)
-    push!(pool, lllll)
-    push!(pool, triangle)
+    #=push!(pool, triangle)=#
+    #=push!(pool, lllll)=#
+    #=push!(pool, triangle)=#
+    #=push!(pool, stick)=#
     push!(pool, stick)
+    push!(pool, stick)
+    push!(pool, lllll)
+    push!(pool, jjjjj)
+    push!(pool, zzzzz)
 
-    solvePool(board, pool)
+    n = 1
+
+    for per in permutations(pool)
+        t1, t2 = tester(board, per)
+        if t1 > 1.0
+            println(per)
+        end
+        println("$n $t1 $t2")
+        n += 1
+    end
+    return
 
     #################################
 
@@ -220,7 +312,7 @@ function main()
     push!(pool, lllll)
     push!(pool, stick)
 
-    solvePool(board, pool)
+    tester(board, pool)
 
     #################################
 
@@ -232,7 +324,7 @@ function main()
     push!(pool, triangle)
     push!(pool, zzzzz)
 
-    solvePool(board, pool)
+    tester(board, pool)
 
     #################################
 
@@ -244,7 +336,7 @@ function main()
     push!(pool, jjjjj)
     push!(pool, zzzzz)
 
-    solvePool(board, pool)
+    tester(board, pool)
 
     #################################
     return
@@ -282,3 +374,5 @@ function main()
     #=push!(pool, lllll)=#
 
 end
+
+main()
